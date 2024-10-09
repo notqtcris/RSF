112316d994e6e53309d4d122e85827cb7e34b4d0
notqtcris <74668771+notqtcris@users.noreply.github.com>
2024-10-09 22:21:00 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 main() {
     sudo apt update
     sudo apt install git pip -y
-    git clone https://github.com/meower1/Reality-SNI-finder.git
-    (cd Reality-SNI-finder && pip install -r requirements.txt) 
-    (cd Reality-SNI-finder && python3 main.py)
+    git clone https://github.com/notqtcris/RSF.git
+    (cd RSF && pip install -r requirements.txt) 
+    (cd RSF && python3 main.py)
 }
 
 main
