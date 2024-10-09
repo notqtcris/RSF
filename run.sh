e1c057c422687e9f5eb888ac6057d098552c9f91
notqtcris <74668771+notqtcris@users.noreply.github.com>
2024-10-09 22:47:57 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 main() {
-    sudo apt update
-    sudo apt install git pip -y
+    apt update
+    apt install git pip -y
     git clone https://github.com/notqtcris/RSF.git
     (cd RSF && pip install -r requirements.txt) 
     (cd RSF && python3 main.py)
